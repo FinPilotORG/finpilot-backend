610c66efd5764ab928c97ae984c0d6c475003074
Vaishnavih1903 <vaishnavihajare19@gmail.com>
2026-08-06 19:33:31 +0530

    feat(auth): create authentication foundation

---
+++ src/main/java/com/finpilot/service/impl/AuthServiceImpl.java
@@ -0,0 +1,9 @@
+package com.finpilot.service.impl;
+
+import com.finpilot.service.AuthService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+}
